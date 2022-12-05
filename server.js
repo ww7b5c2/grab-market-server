51cde78648aa4f73d030f86c2c6b1dc9b463af63
bruce <ww7b5c2@gmail.com>
2022-12-05 13:24:52 +0900

    2022-12-05_server

--- server.js
+++ server.js
@@ -32,22 +32,17 @@ app.get("/products", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("에러 발생");
+      res.status(400).send("에러 발생");
     });
 });
 
 app.post("/products", (req, res) => {
   const body = req.body;
-  const { name, description, price, seller } = body;
-  if (!name || !description || !price || !seller) {
-    res.send("모든 필드를 입력해주세요");
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요");
   }
-  models.Product.create({
-    name,
-    description,
-    price,
-    seller,
-  })
+  models.Product.create({ description, price, seller, imageUrl, name })
     .then((result) => {
       console.log("상품 생성 결과 : ", result);
       res.send({
@@ -56,7 +51,7 @@ app.post("/products", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("상품 업로드에 문제가 발생했습니다");
+      res.status(400).send("상품 업로드에 문제가 발생했습니다");
     });
 });
 
@@ -76,7 +71,7 @@ app.get("/products/:id", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("상품 조회에 에러가 발생했습니다.");
+      res.status(400).send("상품 조회에 에러가 발생했습니다.");
     });
 });
 
